e4c187b2d689049b66360a6fcbd9a38735ac85e3
toguchi takeryu <e165741@ie.u-ryukyu.ac.jp>
2017-01-31 10:37:23 +0900

    プログラミング2レポート７

--- src/main/java/jp/ac/uryukyu/ie/e165741/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165741/Main.java
@@ -4,4 +4,7 @@ package jp.ac.uryukyu.ie.e165741;
  * Created by e1657 on 2017/01/31.
  */
 public class Main {
+    public static void main(String[] args){
+        System.out.println("戦闘終了");
+    }
 }
